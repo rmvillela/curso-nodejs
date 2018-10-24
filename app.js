4dc616e9233cca5be930290bce24b0057fe13026
Roosevelt Villela <rmvillela@outlook.com>
2018-10-24 13:32:30 -0200

    Integração com o consign para que facilite a inclusão de rotas no app

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var app = require('./config/server'); // Importa o módulo server
 
-var rotaHome = require('./app/routes/home')(app); //Retorna a execução da função
-var rotaNoticia = require('./app/routes/noticia')(app); //Retorna a execução da função
-var rotaNoticias = require('./app/routes/noticias')(app); //Retorna a execução da função
-var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app); //Retorna a execução da função
-
 app.listen(3000, function() {
   console.log('teste modafuka');
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 var express = require('express'); // O require do express retorna uma função que não é executada por padrão. Para acionar basta adicionar um () no final da chamada, ficando require('express')();
-var app = express();
+var consign = require('consign');
 
+var app = express();
 app.set('view engine', 'ejs'); // Altera uma propriedade do express
 app.set('views', 'app/views'); // Aponta o caminho das views (é relativo ao local onde o módulo está sendo importado)
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
