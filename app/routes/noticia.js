94b39c43ab076b675b46231a5f67518fc50e1823
Roosevelt Villela <rmvillela@outlook.com>
2018-10-24 22:55:27 -0200

    Criação da rota e da view referente ao detalhamento da notícia

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,5 +1,9 @@
 module.exports = function(app) {
-  app.get('/noticia', function(req, res) {
-    res.render('noticias/noticia');
-  });
+	app.get('/noticia', function(req, res) {
+		var connection = app.config.dbConnection(); // Recupera a função contida na variável exportada pelo módulo dbConnection, executando-a
+
+		connection.query('select * from noticias where id_noticia = 1', function(error, result) {
+			error ? res.send(error) : res.render('noticias/noticia', {noticia: result});
+		});
+	});
 };
